63ab6341fe9111d184f1b6e813b33e37abed3553
Martin Fetzel <mfe@live.de>
2022-06-16 21:34:38 +0200

    upgrade sample to latest dotnet maui version

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    public static Page page;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            count++;
+            CounterBtn.Text = $"Current count: {count}";
+            SemanticScreenReader.Announce(CounterBtn.Text);
+            if (App.Current.MainPage is NavigationPage navigationPage)
+            {
+                if (MainPage.page == null)
+                {
+                    MainPage.page = new NewGamePage();
+                }
+                navigationPage.CurrentPage.Navigation.PushAsync(MainPage.page);
+            }
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine(ex);
+        }
+
+    }
+}
+
---
+++ NewGamePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp2;
+
+public partial class NewGamePage : ContentPage
+{
+	public NewGamePage()
+	{
+		InitializeComponent();
+	}
+}
